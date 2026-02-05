f81fc223b691b57939b3055081be032fe9628892
ThorkildFregi <gabrielbermesp@gmail.com>
2026-02-05 18:32:36 +0100

    Start fisrt page + redirect to lesson page

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, url_for, redirect, render_template, request
+import json
+
+app = Flask(__name__)
+
+with open("./data/subjects.json", "r") as file:
+    subjects = json.load(file)["subjects"]
+
+print(subjects)
+
+@app.route("/")
+def root_to_home():
+    return redirect(url_for("home"))
+
+@app.route("/home")
+def home():
+    return render_template("home.html", subjects=subjects)
+
+@app.route("/lesson", methods=["get"])
+def lesson():
+    if request.method == "GET":
+        folder = request.args.get("folder", type=str)
+        file = request.args.get("file", type=str)
+
+        print(folder, file)
+
+        return ""
+    else:
+        return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
